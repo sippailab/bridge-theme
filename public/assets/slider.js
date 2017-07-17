1eb96f6e097f5c92f67276d7f437a4167cb0fb6d
sakthivel <sakthivelsubbarayan@gmail.com>
2017-07-17 19:11:13 +0530

    90%homepage

---
+++ public/assets/slider.js
@@ -0,0 +1,25 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("img_slide");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+$(document).ready(function() {
+
+    $(".image_caption").addClass("animated fadeOut");
+	});
+	$(document).ready(function()
+	 {
+    $(.image_caption).fadeOut('10000');
+ });
