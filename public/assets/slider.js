1072a9b83c6779dd96f5e860f7134c90c1287516
sakthivel <sakthivelsubbarayan@gmail.com>
2017-07-18 19:13:29 +0530

    completed anout page

--- public/assets/slider.js
+++ public/assets/slider.js
@@ -1,25 +1,21 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("img_slide");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-$(document).ready(function() {
 
-    $(".image_caption").addClass("animated fadeOut");
-	});
-	$(document).ready(function()
-	 {
-    $(.image_caption).fadeOut('10000');
- });
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
