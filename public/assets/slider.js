7c04fcfc5a6337180e5f27c53b1b9a58777ba3ed
sakthivel <sakthivelsubbarayan@gmail.com>
2017-07-19 19:06:22 +0530

    completed business and sport page

--- public/assets/slider.js
+++ public/assets/slider.js
@@ -9,9 +9,10 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("slide");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
